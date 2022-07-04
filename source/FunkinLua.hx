1bfe9887cb12b5b030e57c6c5f38c012c0ec8bd4
bigcoqui <102246267+bigcoqui@users.noreply.github.com>
2022-07-04 09:44:21 -0300

    update FunkinLua.hx

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -1921,8 +1921,6 @@ class FunkinLua
 			luaTrace('musicFadeOut is deprecated! Use soundFadeOut instead.', false, true);
 		});
 
-		Discord.DiscordClient.addLuaCallbacks(lua);
-
 		call('onCreate', []);
 		#end
 	}
