f099ebdb6716e6d269905affbe70dde1985abb67
bigcoqui <102246267+bigcoqui@users.noreply.github.com>
2022-07-05 10:41:22 -0300

    ok

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -1110,7 +1110,7 @@ class ChartingState extends MusicBeatState
 		var tab_group_event = new FlxUI(null, UI_box);
 		tab_group_event.name = 'Events';
 
-		#if LUA_ALLOWED
+		/*#if LUA_ALLOWED
 		var eventPushedMap:Map<String, Bool> = new Map<String, Bool>();
 		var directories:Array<String> = [
 			Paths.mods('custom_events/'),
@@ -1138,7 +1138,7 @@ class ChartingState extends MusicBeatState
 		}
 		eventPushedMap.clear();
 		eventPushedMap = null;
-		#end
+		#end*/
 
 		descText = new FlxText(20, 240, 0, eventStuff[0][0]);
 
