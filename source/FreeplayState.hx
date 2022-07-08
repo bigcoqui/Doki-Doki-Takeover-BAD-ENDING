6a03a8fc8ab450e1c9bdb052a70c576f408b1d4d
bigcoqui <102246267+bigcoqui@users.noreply.github.com>
2022-07-08 18:01:51 -0300

    idk

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -395,10 +395,10 @@ class FreeplayState extends MusicBeatState
 
 		// selector.y = (70 * curSelected) + 30;
 
-		#if !switch
+		/*#if !switch
 		intendedScore = Highscore.getScore(songs[curSelected].songName, curDifficulty);
 		intendedRating = Highscore.getRating(songs[curSelected].songName, curDifficulty);
-		#end
+		#end*/
 
 		var bullShit:Int = 0;
 
